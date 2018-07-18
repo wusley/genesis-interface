57ae31462517ef2595bbe18bde4b781482ddae68
Wesley <wesley.lima@inkuba.com.br>
2018-07-18 02:36:15 -0300

    Adicionado bootstrap

--- src/modules/pages/Home.js
+++ src/modules/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, ControlLabel, Col, FormControl, Checkbox, Button } from 'react-bootstrap';
 
 // Common
 
@@ -9,6 +10,37 @@ export default class Home extends Component {
   render() {
     return (
       <div id="home">
+        <div className="mdc-layout-grid">
+          <div className="mdc-layout-grid__inner">
+            <div className="mdc-layout-grid__cell--span-12-desktop mdc-layout-grid__cell--span-8-tablet mdc-layout-grid__cell--span-4-phone">
+              <Form horizontal>
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    Field 1
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl type="text" placeholder="Field 1" />
+                  </Col>
+                </FormGroup>
+
+                <FormGroup controlId="formHorizontalEmail">
+                  <Col componentClass={ControlLabel} sm={2}>
+                    Field 2
+                  </Col>
+                  <Col sm={10}>
+                    <FormControl type="text" placeholder="Field 2" />
+                  </Col>
+                </FormGroup>
+
+                <FormGroup>
+                  <Col smOffset={2} sm={10}>
+                    <Button type="submit">Send</Button>
+                  </Col>
+                </FormGroup>
+              </Form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
